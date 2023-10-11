dac53c1ed1638c42da21e0cbd045c1f54097deea
Noah Santos <nsanto591@west-mec.org>
2023-10-11 16:31:16 -0700

    Nav
    Made the nav bar

--- public/card.js
+++ public/card.js
@@ -38,7 +38,22 @@ $(function(){
         })
     }
 
-    
+     // when burger is clicked, open nav
+    $("#burger").click(function(){
+        $(".nav").animate({width: "28%"}, 300).delay(50).promise().done(function(){
+            $(".x").css("display", "block");
+            $(".navLink").css("display", "flex");
+        });
+        $("section").css("filter", "blur(4px)");
+    });
+
+    // when x is clicked, close nav
+    $(".x").click(function(){
+        $(".x").css("display", "none");
+        $(".navLink").css("display", "none");
+        $(".nav").animate({width: "0%"}, 300);
+        $("section").css("filter", "blur(0px)");
+    });  
 })
 
 // Declares each set cards
