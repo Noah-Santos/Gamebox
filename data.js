13eea15d0246a623de19596723b0db54b32ddf44
Noah-Santos <noahbsantos@gmail.com>
2023-10-11 21:45:04 -0700

    Deck
    Made it possible to get a new deck from the api and draw a card with being able to get the value and suit of said card

--- data.js
+++ data.js
@@ -9,9 +9,22 @@ require('dotenv').config();
 const connectDB = require('./db/connect');
 
 async function getDeck(){
-    // let {deck_id} = await fetch('https://www.deckofcardsapi.com/api/deck/new/');
+    // get new shuffled deck
     let deck = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').then(response =>{return response.json()});
-    console.log(deck);
+    // console.log(deck);
+
+    // get deck id
+    let deck_id = deck.deck_id;
+    // console.log(deck_id);
+
+    // draw a card
+    let draw = await fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`).then(response =>{return response.json()});
+    // console.log(draw);
+
+    // print value and suit of chosen card
+    let value = draw.cards[0].value;
+    let suit = draw.cards[0].suit;
+    console.log(`${value} of ${suit}`);
 }
 getDeck();
 // console.log(cards);
